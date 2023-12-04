8e686892d7e7c618f97f9d661dda5acbfca56a56
Emmi Lindsey <emmilindsey@gmail.com>
2023-12-04 01:05:26 -0700

    janky but existent pie chart

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,32 @@ import './App.css'
 import Sidebar from "./Sidebar.jsx"
 import ShoeDetails from "./ShoeDetails.jsx"
 import Header from "./Header.jsx"
-import MileageAnalytics from "./MileageAnalytics.jsx"
+// import MileageAnalytics from "./MileageAnalytics.jsx"
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Hoka Clifton 7", "Brooks Launch", "La Sportiva Jackals"],
+  datasets: [
+    {
+      label: 'Mileage',
+      data: [12, 19, 3],
+      backgroundColor: [
+        "#3a4664",
+        "#eaa0a2",
+        "ffdbdc"
+      ],
+      borderColor: [
+        "#3a4664",
+        "#eaa0a2",
+        "ffdbdc"
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 
 
 function App() {
@@ -24,7 +49,7 @@ function App() {
     <div className="main-page">
       <Sidebar shoes={shoes} currentShoe={currentShoe} setCurrentShoe={setCurrentShoe} />  
       <ShoeDetails currentShoe={currentShoe} setCurrentShoe={setCurrentShoe}/>   
-      <MileageAnalytics />
+      <Pie data={data}/>
     </div>
     </>
   )
